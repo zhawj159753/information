c1c4cfd917adc21f08fb5c8e762e471405230653
zhawj159753 <zhawj159753@163.com>
2013-09-18 20:33:33 +0800

    基本信息和工作信息已添加
    工作信息需要javascript辅助完成。目前还未写javascript。

---
+++ js/ui.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+    $("#birth").datepicker({
+    	changeMonth:true,
+    	changeYear:true,
+    	yearRange:'c-100:c',
+        dayNamesMin: ['天', '一', '二', '三', '四', '五', '六'],
+    	monthNamesShort:['1', '2', '3', '4', '5', '6','7','8','9','10','11','12']});
+});
+
+$(document).ready(function() {
+    $("#start_to_work").datepicker({
+    	changeMonth:true,
+    	changeYear:true,
+    	yearRange:'c-100:c',
+        dayNamesMin: ['天', '一', '二', '三', '四', '五', '六'],
+    	monthNamesShort:['1', '2', '3', '4', '5', '6','7','8','9','10','11','12']});
+});
+
+$(document).ready(function() {
+    $("#start_to_current_work").datepicker({
+    	changeMonth:true,
+    	changeYear:true,
+    	yearRange:'c-100:c',
+        dayNamesMin: ['天', '一', '二', '三', '四', '五', '六'],
+    	monthNamesShort:['1', '2', '3', '4', '5', '6','7','8','9','10','11','12']});
+});
+
+$(document).ready(function() {
+    $("#start1").datepicker({
+    	changeMonth:true,
+    	changeYear:true,
+    	yearRange:'c-100:c',
+        dayNamesMin: ['天', '一', '二', '三', '四', '五', '六'],
+    	monthNamesShort:['1', '2', '3', '4', '5', '6','7','8','9','10','11','12']});
+});
+
+$(document).ready(function() {
+    $("#end1").datepicker({
+    	changeMonth:true,
+    	changeYear:true,
+    	yearRange:'c-100:c',
+        dayNamesMin: ['天', '一', '二', '三', '四', '五', '六'],
+    	monthNamesShort:['1', '2', '3', '4', '5', '6','7','8','9','10','11','12']});
+});
+
+$(document).ready(function() {
+    $("#start2").datepicker({
+    	changeMonth:true,
+    	changeYear:true,
+    	yearRange:'c-100:c',
+        dayNamesMin: ['天', '一', '二', '三', '四', '五', '六'],
+    	monthNamesShort:['1', '2', '3', '4', '5', '6','7','8','9','10','11','12']});
+});
+
+$(document).ready(function() {
+    $("#end2").datepicker({
+    	changeMonth:true,
+    	changeYear:true,
+    	yearRange:'c-100:c',
+        dayNamesMin: ['天', '一', '二', '三', '四', '五', '六'],
+    	monthNamesShort:['1', '2', '3', '4', '5', '6','7','8','9','10','11','12']});
+});
